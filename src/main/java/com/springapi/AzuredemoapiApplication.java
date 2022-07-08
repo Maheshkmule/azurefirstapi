026496af3d6eca6aedd2a2e3b3fa554c05634308
Maheshkmule <107538554+Maheshkmule@users.noreply.github.com>
2022-07-08 08:38:22 +0530

    Update AzuredemoapiApplication.java

--- src/main/java/com/springapi/AzuredemoapiApplication.java
+++ src/main/java/com/springapi/AzuredemoapiApplication.java
@@ -1,10 +1,16 @@
 package com.springapi;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AzuredemoapiApplication {
+	@GetMapping("/message")
+	public String message() {
+		return "The application deployed successfully";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzuredemoapiApplication.class, args);
