71f8e223a97990400feabd71b9a8edab08c66769
Maheshkmule <maheshkishanrao@gmail.com>
2022-07-08 08:11:46 +0530

    first commit

---
+++ src/main/java/com/springapi/AzuredemoapiApplication.java
@@ -0,0 +1,13 @@
+package com.springapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzuredemoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzuredemoapiApplication.class, args);
+	}
+
+}
